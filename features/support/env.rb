d3f604a35f4eb569af2b896db92c88aa9415ec34
atrejog-kueski <arturo.trejo@kueski.com>
2022-09-08 23:40:12 -0400

    Sadd html report and headless exec line

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,8 @@ require './config/config'
 
 require_all './page_object'
 
-Capybara.default_driver = :selenium_chrome
-Capybara.page.driver.browser.manage.window.maximize
+# Remove comment in order to run headless
+# Capybara.default_driver = :selenium_chrome_headless
+ Capybara.default_driver = :selenium_chrome
+ Capybara.page.driver.browser.manage.window.maximize
 ASSESSMENT_URL = "file://#{__dir__}/../../QE-index.html"
